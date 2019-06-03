da9ae07753d133371d37d05df70c96487032d2b4
akash <akash619@protonmail.com>
2019-06-03 10:27:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      getImage: ""
+    };
+  }
+  componentGenerate = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => this.setState({ getImage: data.message }))
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card-image">
+          <h1>DOG GENERTOR</h1>
+
+            <img src={this.state.getImage} alt="" />
+          
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left" />
+            <div className="media-content">
+              <button onClick={this.componentGenerate}>Generate Doggo</button>
+              {/* <p class="subtitle is-6">@johnsmith</p> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
